115a71ebc73555fe62b1273a2e2e08262ace50e4
suehah <suehah0000@gmail.com>
2025-07-17 23:57:05 +0900

    25/7/17

--- app/src/main/java/com/example/dispenser/ui/screens/MemberHomeScreen.kt
+++ app/src/main/java/com/example/dispenser/ui/screens/MemberHomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MemberHomeScreen(
     onBack: () -> Unit,
