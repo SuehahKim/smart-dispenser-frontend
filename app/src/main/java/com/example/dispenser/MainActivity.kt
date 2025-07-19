a165e781dbb95aecf3cdaf1051672210c9ca4972
leeda <leedain0228@naver.com>
2025-07-20 04:40:41 +0900

    재료부족 팝업, 기존 팝업 수정

--- app/src/main/java/com/example/dispenser/MainActivity.kt
+++ app/src/main/java/com/example/dispenser/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,13 +11,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.dispenser.navigation.NavGraph
 import com.example.dispenser.ui.theme.DispenserTheme
 
+//import com.example.dispenser.ui.popups.TestStockAlertPopup
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DispenserTheme {
-                    NavGraph()
+//                TestStockAlertPopup() // 🔔 팝업만 단독 실행
+               NavGraph()
             }
         }
     }
